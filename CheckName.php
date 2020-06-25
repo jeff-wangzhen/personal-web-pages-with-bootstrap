99d8f1e6fd723f0953bf9778ca8398f226d36b1c
wangzhen <kill370354@qq.com>
2020-06-25 18:16:39 +0800

    refactor: use pdo when checkname

--- CheckName.php
+++ CheckName.php
@@ -1,17 +1,27 @@
 <?php
 header("Content-type:text/html;charset=utf-8");
-$link=mysqlI_connect("localhost","a0912161003","qaz123","a0912161003");
-mysqli_query($link,"set names utf-8");
-$user_name=$_GET["username"];
-//var_dump($user_name);
-$s="select * from tb_grwzphpusers where username=".$user_name;
-//var_dump($s);
-$sql=mysqli_query($link,$s);
-//var_dump($sql);
-$info=mysqlI_fetch_array($sql);
-if ($info){
-    echo "<p>y</p>";
-}else{
- echo "<p>no</p>";
+
+include 'Connect.php';
+
+// var_dump($_GET);
+$a = $_GET["username"];
+
+$sql = "select * from tb_grwzphpusers where username=?";
+$result = $pdo->prepare($sql);
+$result->bindParam(1, $a);
+
+
+try {
+    // var_dump($result);
+    $pdo->beginTransaction();
+    $result->execute();
+    $row = $result->fetchAll(PDO::FETCH_ASSOC);
+    if ($row) {
+        echo "<p>y</p>";
+    } else {
+        echo "<p>no</p>";
+    }
+    $pdo->commit();
+} catch (PDOException $e) {
+    echo $e;
 }
-?>
